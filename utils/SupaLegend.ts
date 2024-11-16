eb0f90d877fe8492f85d1354e73953ac5745d499
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-16 12:48:33 +0000

    build works with jdk v17

--- utils/SupaLegend.ts
+++ utils/SupaLegend.ts
@@ -3,13 +3,13 @@ import { observable } from "@legendapp/state";
 import { syncedSupabase } from "@legendapp/state/sync-plugins/supabase";
 import { configureSynced } from "@legendapp/state/sync";
 import { observablePersistAsyncStorage } from "@legendapp/state/persist-plugins/async-storage";
-import { ObservablePersistLocalStorage} from '@legendapp/state/persist-plugins/local-storage';
+import { ObservablePersistLocalStorage } from "@legendapp/state/persist-plugins/local-storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Database } from './database.types'
-import 'react-native-get-random-values'
-import { v4 as uuidv4 } from 'uuid'
+import { Database } from "./database.types";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
 
-const generateId = () => uuidv4()
+const generateId = () => uuidv4();
 
 const supabase = createClient<Database>(
   process.env.EXPO_PUBLIC_SUPABASE_URL!,
@@ -20,7 +20,7 @@ const supabase = createClient<Database>(
 const customSynced = configureSynced(syncedSupabase, {
   // Use React Native Async Storage
   persist: {
-    plugin: typeof window ? ObservablePersistLocalStorage : observablePersistAsyncStorage({
+    plugin: observablePersistAsyncStorage({
       AsyncStorage,
     }),
   },
@@ -53,14 +53,14 @@ export const todos$ = observable(
 );
 
 export function addTodo(text: string) {
-  const id = generateId()
+  const id = generateId();
   // Add keyed by id to the todos$ observable to trigger a create in Supabase
   todos$[id].assign({
     id,
     text,
-  })
+  });
 }
 
 export function toggleDone(id: string) {
-  todos$[id].done.set((prev) => !prev)
+  todos$[id].done.set((prev) => !prev);
 }
