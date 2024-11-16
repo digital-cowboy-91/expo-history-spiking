622a9a7cd0151b74f966abec02943cff372f3029
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-16 19:29:47 +0000

    add bottom margin

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Text } from "react-native";
 
 export default function PageTitle({ children }) {
-  return <Text className="text-2xl font-bold">{children}</Text>;
+  return <Text className="text-2xl font-bold mb-5">{children}</Text>;
 }
